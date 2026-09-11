703c6ef0e1926b3587eabdc0d4d3c67841c53130
Harsha <kumarharsha980@gmail.com>
2026-09-11 23:52:43 +0530

    wire framing and securities are wired up , idk what i'm writing

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -17,3 +17,28 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.urlencoded({extended:true}));
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'session-fallback',
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24,
+        sameSite:'lax',
+    },
+
+}));
+app.get('/health',(req: Request, res: Response)=>{
+    res.json(200).json({
+        sucess: true,
+        status:' online',
+        message:'serveris running cool'
+
+
+    }); 
+});
+app.listen(PORT,() =>{
+    console.log('finally your so called hand written code is working fine better not mess up the future');
+    console.log(`check the fucking port ${PORT}for the number`);
+});
