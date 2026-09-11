2e02e7dc60f82183c8a1da1ee1d781899ca89369
Harsha <kumarharsha980@gmail.com>
2026-09-12 01:04:05 +0530

    i missed something to save

--- Backend/src/services/github.service.ts
+++ Backend/src/services/github.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 export class GitHubService{
     static async getPullreq( owner:string , repo:string , pr:number, token:string ): Promise<string>{
         try {
-            const response = await axios.get('https://api.github.com/repos/${owner}/${repo}/pulls/${pr}',{
+            const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/pulls/${pr}`,{
                 headers:{
                     Authorization :`token ${token}`,
                     Accept: 'application/vnd.github.v3diff',
