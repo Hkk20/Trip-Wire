3cf684f58efafae4c906a2fdd5e67b4ff654e38d
Harsha <kumarharsha980@gmail.com>
2026-09-11 19:56:08 +0530

    configuring dependencies

---
+++ Backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const app: Application = express();
+const PORT = process.env.PORT || 8080;
+app.use(helmet());
+app.use(cors({
+    origin:true,
+    credentials:true
+}));
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.urlencoded({extended:true}));
